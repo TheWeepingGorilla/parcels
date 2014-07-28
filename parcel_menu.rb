987cb0935d6f04377f486049955770df621db020
Sean Morrison & Sterling Houghton <student@epicodus.com>
2014-07-28 12:03:27 -0700

    added main menu

--- parcel_menu.rb
+++ parcel_menu.rb
@@ -18,6 +18,29 @@ def add_parcel
   puts "Shipping cost: #{@parcel_list[-1].cost_to_ship}"
 end
 
+def main_menu
+  loop do
+    p "Press 'a' to add a Parcel or 'l' to see parcel list."
+    p "Press 'x' to exit."
+    main_choice = gets.chomp
+    if main_choice == 'a'
+      add_parcel
+    elsif main_choice == 'l'
+      list_parcels
+    elsif main_choice == 'x'
+      p "Good-bye Ol' Chap"
+      exit
+    else
+      p "That wasn't a valid input"
+    end
+  end
+end
 
+def list_parcels
+  p "Here are your Packages:"
+  @parcel_list.each do |parcel|
+    puts "#{parcel.name}, #{parcel.cost_to_ship}"
+  end
+end
 
-
+main_menu
